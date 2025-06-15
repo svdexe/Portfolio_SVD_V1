e7412bc10ce635002dca7c1e375ae4e9a3a4aba1
SVD <shivamdali@gmail.com>
2025-06-15 15:21:14 +0930

    Randdap Start X92

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -196,7 +196,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/WeezyXX.jpg.bak"
+              src="../../images/WeezyXX.jpg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
