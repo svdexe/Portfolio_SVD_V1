f6fa122e057035f3ef19fd09933b6f4fa976f935
SVD <shivamdali@gmail.com>
2025-06-18 02:24:46 +0930

    Succesful Deployment SVD Vercel 18/06/2025

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -196,7 +196,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/WeezyXX.jpg"
+              src="../../images/weezy1.jpg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
